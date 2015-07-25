7c8fba34d94182ebdcdbbab1524d9870b4244c2c
yayanyang <yayanyang@gmail.com>
2015-07-25 10:17:38 +0800

    update

--- ast/expr.go
+++ ast/expr.go
@@ -1,6 +1,36 @@
 package ast
 
 // Expr instruction
-type Expr struct {
-	_Node // mixin _Node
+type Expr interface {
+	Node // Mixin Node
+}
+
+// ArgsTable .
+type ArgsTable struct {
+	_Node        // Mixin default node implement
+	args  []Expr // arg list
+}
+
+// NewArgsTable .
+func NewArgsTable() *ArgsTable {
+	args := &ArgsTable{}
+
+	args._init("ArgsTable")
+
+	return args
+}
+
+// Append .
+func (args *ArgsTable) Append(expr Expr) {
+	args.args = append(args.args, expr)
+}
+
+// Arg .
+func (args *ArgsTable) Arg(index int) Expr {
+	return args.args[index]
+}
+
+// Args .
+func (args *ArgsTable) Args() int {
+	return len(args.args)
 }
